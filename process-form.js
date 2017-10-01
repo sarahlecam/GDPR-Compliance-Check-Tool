d4d6de3d659818132b2b2f23e0b537d11dba1173
Jake Magid <jake.magid@gmail.com>
2017-10-01 18:21:04 -0400

    testing

---
+++ process-form.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    console.log("READY");
+    // var newDiv = makeElement();
+    // $( "#nameMsg" ).hide();
+    // $(newDiv).hide();
+    // $( "#name" ).focusin(function() {
+    //     show(newDiv);
+    // });
+    // $( "#name" ).focusout(function() {
+    //     hide(newDiv);
+    // });
+});
+
+
+function show(dived) {
+    $(dived).show();
+}
+
+function hide(dived) {
+    $(dived).hide();
+}
+
+function makeElement() {
+    var nameBox = document.createElement("div");
+    nameBox.setAttribute("id", "nameMsg");
+    // nameBox.setAttribute("");
+    nameBox.innerHTML = "Your name is being collected to able to murder you in the future !!!";
+    $(nameBox).appendTo(document.body);
+}
